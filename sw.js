3cd670d796de2a85f7133fae6ab731e21f3d2d14
yutsutke <127080972+yutsutke@users.noreply.github.com>
2026-05-09 14:47:44 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@
 //   2. GitHubにpush
 //   3. ユーザーがハードリロード or アプリ再起動で更新が反映される
 
-const CACHE_VERSION = 'v18';
+const CACHE_VERSION = 'v19';
 const CACHE_NAME = `norireco-${CACHE_VERSION}`;
 
 // 起動時にプリキャッシュする静的アセット
